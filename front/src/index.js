0e799821a5ad5ef56da728892863f2c1ed2badd6
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2023-03-13 15:32:15 +0100

    fix erreur

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Scss/style.css';
+import './Scss/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
